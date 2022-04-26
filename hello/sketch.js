d87f4cb3d700a5eabf9cbf54e8301fcd14f087c5
Eli Pleaner <eliipleaner@gmail.com>
2022-04-26 13:55:22 -0700

    Update sketch.js

--- hello/sketch.js
+++ hello/sketch.js
@@ -3,7 +3,7 @@ let fontSize;
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   a = createA('/hello', 'check me out');
-  a.position(width / 2, height / 2);
+  a.position(mouseX, mouseY);
   a.style('font-size', (mouseX+mouseY)/2+'px');
   a.style('color', 'papayawhip');
   a.style('text-decoration', 'none')
@@ -11,8 +11,9 @@ function setup() {
 
 function draw() {
   background(Math.random() * 255, Math.random() * 255, Math.random() * 255,);
-  a.style('font-size', (mouseX+mouseY)/2+'px');
+  a.style('font-size', max((mouseX+mouseY)/8, 10) +'px');
   a.html("you've been on this page for " + millis() / 1000 + " seconds")
+  a.position(mouseX, mouseY);
 }
 
 function windowResized() {
